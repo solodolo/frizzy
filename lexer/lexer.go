a509ef2862192e439ae049acd94eece31ae73df2
Drew Mettlach <dmmettlach@gmail.com>
2021-01-14 17:39:46 +0000

    Parse out remaining tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,6 +2,7 @@ package lexer
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"regexp"
 )
@@ -40,18 +41,19 @@ var logicOp = regexp.MustCompile(`^\|\||&&`)
 var assignOp = regexp.MustCompile(`^=`)
 var unaryOp = regexp.MustCompile(`^!|-`)
 
-var ident = regexp.MustCompile(`^[a-zA-Z]+[a-zA-Z0-9_]*`)
-var varName = regexp.MustCompile(`^([a-zA-Z]+[a-zA-Z0-9_]*)(\.[a-zA-Z][a-zA-Z0-9_]*)*`)
+var identExp = regexp.MustCompile(`^[a-zA-Z]+[a-zA-Z0-9_]*`)
+var varExp = regexp.MustCompile(`^([a-zA-Z]+[a-zA-Z0-9_]*)(\.[a-zA-Z][a-zA-Z0-9_]*)*`)
 var strExp = regexp.MustCompile(`^"[^"]*"`)
 var numExp = regexp.MustCompile(`^[0-9]+`)
 
-var ifExp = regexp.MustCompile(`^if`)
-var elseIfExp = regexp.MustCompile(`^else_if`)
-var elseExp = regexp.MustCompile(`^else`)
+// These can be captured by identExp
+// var ifExp = regexp.MustCompile(`^if`)
+// var elseIfExp = regexp.MustCompile(`^else_if`)
+// var elseExp = regexp.MustCompile(`^else`)
 
-var forExp = regexp.MustCompile(`^for`)
-var inExp = regexp.MustCompile(`^in`)
-var endExp = regexp.MustCompile(`^end`)
+// var forExp = regexp.MustCompile(`^for`)
+// var inExp = regexp.MustCompile(`^in`)
+// var endExp = regexp.MustCompile(`^end`)
 
 var boolExp = regexp.MustCompile(`^true|false`)
 
@@ -69,8 +71,14 @@ func LexFile(file *os.File, tokChan chan []Token) {
 	// Send lines to channel
 	lineChan := make(chan string)
 	go func() {
+		defer close(lineChan)
 		for scanner.Scan() {
 			lineChan <- scanner.Text()
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading lines for lexing: %s\n", err.Error())
+			os.Exit(1)
 		}
 	}()
 
@@ -87,6 +95,7 @@ func LexFile(file *os.File, tokChan chan []Token) {
 func getLineTokens(line string) []Token {
 	tokens := make([]Token, 0)
 
+	// Check each regex against the line
 	for len(line) > 0 {
 		if loc := multOp.FindStringIndex(line); loc != nil {
 			operator, remaining := extractToken(loc, line)
@@ -94,7 +103,73 @@ func getLineTokens(line string) []Token {
 			token := OpToken{Operator: operator}
 			tokens = append(tokens, token)
 		} else if loc := addOp.FindStringIndex(line); loc != nil {
-
+			operator, remaining := extractToken(loc, line)
+			line = remaining
+			token := OpToken{Operator: operator}
+			tokens = append(tokens, token)
+		} else if loc := relOp.FindStringIndex(line); loc != nil {
+			operator, remaining := extractToken(loc, line)
+			line = remaining
+			token := OpToken{Operator: operator}
+			tokens = append(tokens, token)
+		} else if loc := logicOp.FindStringIndex(line); loc != nil {
+			operator, remaining := extractToken(loc, line)
+			line = remaining
+			token := OpToken{Operator: operator}
+			tokens = append(tokens, token)
+		} else if loc := assignOp.FindStringIndex(line); loc != nil {
+			operator, remaining := extractToken(loc, line)
+			line = remaining
+			token := OpToken{Operator: operator}
+			tokens = append(tokens, token)
+		} else if loc := unaryOp.FindStringIndex(line); loc != nil {
+			operator, remaining := extractToken(loc, line)
+			line = remaining
+			token := OpToken{Operator: operator}
+			tokens = append(tokens, token)
+		} else if loc := strExp.FindStringIndex(line); loc != nil {
+			str, remaining := extractToken(loc, line)
+			line = remaining
+			token := StrToken{Str: str}
+			tokens = append(tokens, token)
+		} else if loc := numExp.FindStringIndex(line); loc != nil {
+			num, remaining := extractToken(loc, line)
+			line = remaining
+			token := NumToken{Num: num}
+			tokens = append(tokens, token)
+		} else if loc := boolExp.FindStringIndex(line); loc != nil {
+			boolVal, remaining := extractToken(loc, line)
+			line = remaining
+			token := BoolToken{Value: boolVal}
+			tokens = append(tokens, token)
+		} else if loc := varExp.FindStringIndex(line); loc != nil {
+			variable, remaining := extractToken(loc, line)
+			line = remaining
+			token := VarToken{Variable: variable}
+			tokens = append(tokens, token)
+		} else if loc := identExp.FindStringIndex(line); loc != nil {
+			// Ident should come after more specific tokens like bool and var
+			ident, remaining := extractToken(loc, line)
+			line = remaining
+			token := IdentToken{Identifier: ident}
+			tokens = append(tokens, token)
+		} else if loc := symbolExp.FindStringIndex(line); loc != nil {
+			symbol, remaining := extractToken(loc, line)
+			line = remaining
+			token := SymbolToken{Symbol: symbol}
+			tokens = append(tokens, token)
+		} else if loc := blockExp.FindStringIndex(line); loc != nil {
+			block, remaining := extractToken(loc, line)
+			line = remaining
+			token := BlockToken{Block: block}
+			tokens = append(tokens, token)
+		} else {
+			// No match so just pass through the char at the front
+			// of the line
+			val, remaining := extractToken([]int{0, 1}, line)
+			line = remaining
+			token := PassthroughToken{Value: val}
+			tokens = append(tokens, token)
 		}
 	}
 	return tokens
