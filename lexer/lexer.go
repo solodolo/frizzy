fe5febe790be09a3c12beeffbc96aa37fef2d586
Drew Mettlach <dmmettlach@gmail.com>
2021-01-15 17:49:28 +0000

    Break input lines into block and non-block text

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 )
 
 // Theses are the tokens of our grammar
@@ -36,10 +37,10 @@ import (
 
 var multOp = regexp.MustCompile(`^[*\/%]`)
 var addOp = regexp.MustCompile(`^[+-]`)
-var relOp = regexp.MustCompile(`^>=|<=|!=|==|<|>`)
-var logicOp = regexp.MustCompile(`^\|\||&&`)
+var relOp = regexp.MustCompile(`^(>=|<=|!=|==|<|>)`)
+var logicOp = regexp.MustCompile(`^(\|\||&&)`)
 var assignOp = regexp.MustCompile(`^=`)
-var unaryOp = regexp.MustCompile(`^!|-`)
+var unaryOp = regexp.MustCompile(`^!`)
 
 var identExp = regexp.MustCompile(`^[a-zA-Z]+[a-zA-Z0-9_]*`)
 var varExp = regexp.MustCompile(`^([a-zA-Z]+[a-zA-Z0-9_]*)\.[a-zA-Z][a-zA-Z0-9_]*`)
@@ -55,17 +56,18 @@ var numExp = regexp.MustCompile(`^[0-9]+`)
 // var inExp = regexp.MustCompile(`^in`)
 // var endExp = regexp.MustCompile(`^end`)
 
-var boolExp = regexp.MustCompile(`^true|false`)
+var boolExp = regexp.MustCompile(`^(true|false)`)
 
 var symbolExp = regexp.MustCompile(`^[;()]`)
 
-var blockExp = regexp.MustCompile(`^{{:|{{|}}`)
+var blockExp = regexp.MustCompile(`^({{:|{{|}})`)
+
+var spaceExp = regexp.MustCompile(`^ +`)
 
 // Runs through file and creates a stream of tokens
 // from the input
-func LexFile(file *os.File, tokChan chan []Token) {
-	// Read text line by line
-	scanner := bufio.NewScanner(file)
+func LexFile(scanner *bufio.Scanner, tokChan chan []Token) {
+	// Read line by line
 	scanner.Split(bufio.ScanLines)
 
 	// Send lines to channel
@@ -84,9 +86,109 @@ func LexFile(file *os.File, tokChan chan []Token) {
 
 	// Read as lines come into the channel and
 	// convert into a slice of Tokens
+	openBlock := false
 	for line := range lineChan {
-		tokChan <- getLineTokens(line)
+		tokens, stillOpen := processLine(line, openBlock)
+		openBlock = stillOpen
+		tokChan <- tokens
 	}
+}
+
+// All text that comes before a {{: or {{ is passed through
+// and all text that comes after the corresponding }} is
+// also passed through
+func processLine(line string, openBlock bool) ([]Token, bool) {
+	// Check for indices of open and close blocks
+	start, end := getBlockIndices(line)
+	// Get text that should be passed through vs fully tokenized
+	preBlock, inBlock, postBlock := partitionLine(line, start, end, openBlock)
+
+	lineTokens := []Token{}
+
+	// If there is text before block, pass it through
+	if len(preBlock) > 0 {
+		token := PassthroughToken{Value: preBlock}
+		lineTokens = append(lineTokens, token)
+	}
+
+	// Tokenize the text in the blocks including any open/close blocks
+	lineTokens = append(lineTokens, getLineTokens(inBlock)...)
+
+	// If there is text after block, pass it through
+	if len(postBlock) > 0 {
+		token := PassthroughToken{Value: postBlock}
+		lineTokens = append(lineTokens, token)
+	}
+
+	return lineTokens, isBlockStillOpen(start, end, openBlock)
+}
+
+/* Returns the indices for the open and close blocks in line
+* Examples
+* {{abcde}} => 0,9
+* abc{{de}} => 3,9
+* ab{{cd}}e => 2,8
+* {{abcde => 0,7
+* abcde}} => 0,7
+* a{{bcde => 1,7
+* abcd}}e => 0,6 */
+func getBlockIndices(line string) (int, int) {
+	openingBlocks := []string{"{{:", "{{"}
+	openingIndex, closingIndex := -1, -1
+
+	for _, block := range openingBlocks {
+		if i := strings.Index(line, block); i != -1 {
+			openingIndex = i
+			break
+		}
+	}
+
+	if i := strings.Index(line, "}}"); i != -1 {
+		closingIndex = i + 2 // + 2 to include '}}'
+	}
+
+	return openingIndex, closingIndex
+}
+
+// Partitions the line into text before opening block text in block and
+// text after closing block.
+// Considers if there was an open block in some previous line.
+func partitionLine(line string, start, end int, openBlock bool) (string, string, string) {
+	var preBlock, inBlock, postBlock string
+	if openBlock {
+		if end == -1 {
+			inBlock = line
+		} else {
+			inBlock = line[:end]
+			postBlock = line[end:]
+		}
+	} else {
+		if start == -1 {
+			preBlock = line
+		} else if start != -1 && end != -1 {
+			preBlock = line[:start]
+			inBlock = line[start:end]
+			postBlock = line[end:]
+		} else if start != -1 {
+			preBlock = line[:start]
+			inBlock = line[start:]
+		}
+	}
+
+	return preBlock, inBlock, postBlock
+}
+
+// Determines the new open block status based on current
+// status and the presence of open and/or close blocks in
+// the current line
+func isBlockStillOpen(start, end int, openBlock bool) bool {
+	if openBlock && end != -1 {
+		openBlock = false
+	} else if !openBlock && start != -1 && end == -1 {
+		openBlock = true
+	}
+
+	return openBlock
 }
 
 // Check the front of the line for each of the tokens
@@ -97,6 +199,7 @@ func getLineTokens(line string) []Token {
 
 	// Check each regex against the line
 	for len(line) > 0 {
+		line = spaceExp.ReplaceAllString(line, "") // Ignore leading spaces
 		if loc := multOp.FindStringIndex(line); loc != nil {
 			operator, remaining := extractToken(loc, line)
 			line = remaining
@@ -163,7 +266,7 @@ func getLineTokens(line string) []Token {
 			line = remaining
 			token := BlockToken{Block: block}
 			tokens = append(tokens, token)
-		} else {
+		} else if len(line) > 0 {
 			// No match so just pass through the char at the front
 			// of the line
 			val, remaining := extractToken([]int{0, 1}, line)
