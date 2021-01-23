38ad618a666ae3f4b66d669318a6bbcee56cfd57
Drew Mettlach <dmmettlach@gmail.com>
2021-01-23 04:42:48 +0000

    Add parser logic for reductions

--- parser/parser.go
+++ parser/parser.go
@@ -7,8 +7,9 @@ import (
 	"mettlach.codes/frizzy/lexer"
 )
 
-func Parse(tokChan chan []lexer.Token, nodeChan chan ParentNode) {
+func Parse(tokChan chan []lexer.Token, nodeChan chan TreeNode) {
 	stateStack := &[]int{}
+	nodeStack := &[]TreeNode{}
 
 	for tokens := range tokChan {
 		// When open block is found
@@ -22,7 +23,7 @@ func Parse(tokChan chan []lexer.Token, nodeChan chan ParentNode) {
 				nodeChan <- node
 			} else {
 				tokSlice := tokens[i:]
-				err := parseTokens(tokSlice, stateStack, nodeChan)
+				err := parseTokens(tokSlice, stateStack, nodeStack, nodeChan)
 
 				if err != nil {
 					// handle error
@@ -32,14 +33,15 @@ func Parse(tokChan chan []lexer.Token, nodeChan chan ParentNode) {
 	}
 }
 
-func parseTokens(tokens []lexer.Token, stateStack *[]int, nodeChan chan ParentNode) (err error) {
+func parseTokens(tokens []lexer.Token, stateStack *[]int, nodeStack *[]TreeNode, nodeChan chan TreeNode) (err error) {
 	if len(*stateStack) == 0 {
 		*stateStack = append(*stateStack, 0)
 	}
 
 	currentState := (*stateStack)[len(*stateStack)-1]
 
-	for _, token := range tokens {
+	for i := 0; i < len(tokens); {
+		token := tokens[i]
 		currentSymbol := token.GetGrammarSymbol()
 		col, exists := SymbolColMapping[currentSymbol]
 
@@ -60,9 +62,77 @@ func parseTokens(tokens []lexer.Token, stateStack *[]int, nodeChan chan ParentNo
 				err = fmt.Errorf("could not convert %q to a valid state", action[1:])
 				break
 			}
+
 			*stateStack = append(*stateStack, nextState)
+
+			// Add tree node to stack
+			node := getNodeForToken(token)
+			*nodeStack = append(*nodeStack, node)
+
+			// go to next token
+			i++
+		} else if IsReduceAction(action) {
+			ruleNum, e := strconv.Atoi(action[1:])
+
+			if e != nil {
+				err = fmt.Errorf("could not convert %q to a valid grammar rule", action[1:])
+				break
+			}
+
+			left, right := GetProductionParts(ruleNum)
+			// Number of symbols to pop
+			numToPop := len(right)
+			*stateStack = (*stateStack)[:len(*stateStack)-numToPop]
+			*nodeStack = (*nodeStack)[:len(*nodeStack)-numToPop]
+
+			// Get the row and column to lookup in goto table
+			lookupState := (*stateStack)[len(*stateStack)-1]
+			lookupCol, exists := SymbolColMapping[left]
+
+			if !exists {
+				err = fmt.Errorf("parse table column not found for symbol %q", left)
+			}
+
+			gotoState, e := strconv.Atoi(LR1ParseTable[lookupState][lookupCol])
+
+			if e != nil {
+				err = fmt.Errorf("could not convert goto state %q to valid state", gotoState)
+				break
+			}
+
+			// Push goto state on top of stack
+			*stateStack = append(*stateStack, gotoState)
+
+			// Push nonterminal onto stack
+			node := &StringParseNode{Value: left}
+			*nodeStack = append(*nodeStack, node)
 		}
 	}
 
 	return
+}
+
+// Creates the appropriate tree node for a given token
+func getNodeForToken(token lexer.Token) TreeNode {
+	var node TreeNode
+
+	switch tok := token.(type) {
+	case lexer.NumToken:
+		num, _ := strconv.Atoi(tok.Num)
+		node = &NumParseNode{Value: num}
+	case lexer.BoolToken:
+		truthy := tok.Value == "true"
+		node = &BoolParseNode{Value: truthy}
+	case lexer.IdentToken:
+		ident := tok.Identifier
+		node = &IdentParseNode{Value: ident}
+	case lexer.VarToken:
+		varName := tok.Variable
+		node = &VarParseNode{Value: varName}
+	default:
+		str := tok.GetValue()
+		node = &StringParseNode{Value: str}
+	}
+
+	return node
 }
