d8753100ff66be77130a4122c1afe6983216edf8
Drew Mettlach <dmmettlach@gmail.com>
2021-01-24 04:10:31 +0000

    Break out shift and reduce methods and add logic for accept action

--- parser/parser.go
+++ parser/parser.go
@@ -7,7 +7,12 @@ import (
 	"mettlach.codes/frizzy/lexer"
 )
 
-func Parse(tokChan chan []lexer.Token, nodeChan chan TreeNode) {
+// Parse reads tokens from tokChan and parses them into tree nodes
+// which are then fed to nodeChan
+func Parse(tokChan chan []lexer.Token, nodeChan chan TreeNode, errChan chan error) {
+	defer close(nodeChan)
+	defer close(errChan)
+
 	stateStack := &[]int{}
 	nodeStack := &[]TreeNode{}
 
@@ -16,32 +21,37 @@ func Parse(tokChan chan []lexer.Token, nodeChan chan TreeNode) {
 		// 	start in table state 0
 		// 	parse until an accept is reached
 		// Go until open block is found
-		for i, token := range tokens {
+		i := 0
+		for i < len(tokens) {
+			token := tokens[i]
 			// Passthrough tokens can just be sent on
 			if ptToken, ok := token.(lexer.PassthroughToken); ok {
-				node := &StringParseNode{Value: ptToken.GetValue()}
+				node := StringParseNode{Value: ptToken.GetValue()}
 				nodeChan <- node
+				i++
 			} else {
-				tokSlice := tokens[i:]
-				err := parseTokens(tokSlice, stateStack, nodeStack, nodeChan)
+				j, err := parseTokens(tokens[i:], stateStack, nodeStack, nodeChan)
 
 				if err != nil {
-					// handle error
+					errChan <- err
 				}
+
+				i += j
 			}
 		}
 	}
+
+	errChan <- nil
 }
 
-func parseTokens(tokens []lexer.Token, stateStack *[]int, nodeStack *[]TreeNode, nodeChan chan TreeNode) (err error) {
+func parseTokens(tokens []lexer.Token, stateStack *[]int, nodeStack *[]TreeNode, nodeChan chan TreeNode) (i int, err error) {
 	if len(*stateStack) == 0 {
 		*stateStack = append(*stateStack, 0)
 	}
 
-	currentState := (*stateStack)[len(*stateStack)-1]
-
-	for i := 0; i < len(tokens); {
+	for i < len(tokens) {
 		token := tokens[i]
+		currentState := (*stateStack)[len(*stateStack)-1]
 		currentSymbol := token.GetGrammarSymbol()
 		col, exists := SymbolColMapping[currentSymbol]
 
@@ -53,63 +63,102 @@ func parseTokens(tokens []lexer.Token, stateStack *[]int, nodeStack *[]TreeNode,
 		action := LR1ParseTable[currentState][col]
 
 		if action == "" {
-			err = fmt.Errorf("invalid action taken for %d, %q", currentState, currentSymbol)
+			err = fmt.Errorf("invalid action taken for state %d, symbol %q", currentState, currentSymbol)
 			break
 		} else if IsShiftAction(action) {
-			nextState, e := strconv.Atoi(action[1:])
+			err = handleShiftAction(action, token, stateStack, nodeStack)
 
-			if e != nil {
-				err = fmt.Errorf("could not convert %q to a valid state", action[1:])
+			if err != nil {
 				break
 			}
-
-			*stateStack = append(*stateStack, nextState)
-
-			// Add tree node to stack
-			node := getNodeForToken(token)
-			*nodeStack = append(*nodeStack, node)
 
 			// go to next token
 			i++
 		} else if IsReduceAction(action) {
-			ruleNum, e := strconv.Atoi(action[1:])
+			err = handleReduceAction(action, stateStack, nodeStack)
 
-			if e != nil {
-				err = fmt.Errorf("could not convert %q to a valid grammar rule", action[1:])
+			if err != nil {
 				break
 			}
+		} else {
+			// Accept is the only remaining option
+			head := NonTerminalParseNode{Value: "A"}
+			head.children = *nodeStack
 
-			left, right := GetProductionParts(ruleNum)
-			// Number of symbols to pop
-			numToPop := len(right)
-			*stateStack = (*stateStack)[:len(*stateStack)-numToPop]
-			poppedNodes := (*nodeStack)[len(*nodeStack)-numToPop:]
-			*nodeStack = (*nodeStack)[:len(*nodeStack)-numToPop]
+			// Send head to channel
+			go func() {
+				nodeChan <- head
+			}()
 
-			// Get the row and column to lookup in goto table
-			lookupState := (*stateStack)[len(*stateStack)-1]
-			lookupCol, exists := SymbolColMapping[left]
-
-			if !exists {
-				err = fmt.Errorf("parse table column not found for symbol %q", left)
-			}
-
-			gotoState, e := strconv.Atoi(LR1ParseTable[lookupState][lookupCol])
-
-			if e != nil {
-				err = fmt.Errorf("could not convert goto state %q to valid state", gotoState)
-				break
-			}
-
-			// Push goto state on top of stack
-			*stateStack = append(*stateStack, gotoState)
-
-			// Push nonterminal onto stack
-			node := &StringParseNode{Value: left}
-			node.children = poppedNodes
-			*nodeStack = append(*nodeStack, node)
+			// Clear stacks
+			*stateStack = []int{}
+			*nodeStack = []TreeNode{}
+			i++
+			break
 		}
 	}
+
+	return
+}
+
+// Shifts the next state onto the state stack and creates a tree node
+// for the symbol
+func handleShiftAction(action string, token lexer.Token, stateStack *[]int, nodeStack *[]TreeNode) (err error) {
+	nextState, e := strconv.Atoi(action[1:])
+
+	if e != nil {
+		err = fmt.Errorf("could not convert %q to a valid state", action[1:])
+		return
+	}
+
+	*stateStack = append(*stateStack, nextState)
+
+	// Add tree node to stack
+	node := getNodeForToken(token)
+	*nodeStack = append(*nodeStack, node)
+
+	return
+}
+
+// Looks up reduction rule A -> B and shifts |B| symbols off each stack.
+// Then pushes GOTO[stateStack.back(), A] onto state stack and A onto node stack
+func handleReduceAction(action string, stateStack *[]int, nodeStack *[]TreeNode) (err error) {
+	ruleNum, e := strconv.Atoi(action[1:])
+
+	if e != nil {
+		err = fmt.Errorf("could not convert %q to a valid grammar rule", action[1:])
+		return
+	}
+
+	left, right := GetProductionParts(ruleNum)
+	// Number of symbols to pop
+	numToPop := len(right)
+	*stateStack = (*stateStack)[:len(*stateStack)-numToPop]
+	poppedNodes := (*nodeStack)[len(*nodeStack)-numToPop:]
+	*nodeStack = (*nodeStack)[:len(*nodeStack)-numToPop]
+
+	// Get the row and column to lookup in goto table
+	lookupState := (*stateStack)[len(*stateStack)-1]
+	lookupCol, exists := SymbolColMapping[left]
+
+	if !exists {
+		err = fmt.Errorf("parse table column not found for symbol %q", left)
+	}
+
+	gotoState, e := strconv.Atoi(LR1ParseTable[lookupState][lookupCol])
+
+	if e != nil {
+		err = fmt.Errorf("could not convert goto state %q to valid state", gotoState)
+		return
+	}
+
+	// Push goto state on top of stack
+	*stateStack = append(*stateStack, gotoState)
+
+	// Push nonterminal onto stack
+	node := &NonTerminalParseNode{Value: left}
+	node.children = poppedNodes
+	*nodeStack = append(*nodeStack, node)
 
 	return
 }
