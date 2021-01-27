376c9e6e9d29b654eb2ad626de273f29516ccae8
Drew Mettlach <dmmettlach@gmail.com>
2021-01-27 05:09:07 +0000

    Add more parser tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 
 	"mettlach.codes/frizzy/lexer"
@@ -50,7 +51,7 @@ func TestPassthroughTokensReturnCorrectNodeTypes(t *testing.T) {
 }
 
 func TestNonPassthroughTokensReturnCorrectNodeTypes(t *testing.T) {
-	var testing = []struct {
+	var tests = []struct {
 		tokens [][]lexer.Token
 		nodes  []TreeNode
 	}{
@@ -162,24 +163,92 @@ func TestNonPassthroughTokensReturnCorrectNodeTypes(t *testing.T) {
 				NonTerminalParseNode{},
 			},
 		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{"},
+					lexer.BlockToken{Block: "}}"},
+					lexer.EOLToken{},
+					lexer.PassthroughToken{},
+				},
+				{
+					lexer.BlockToken{Block: "{{:"},
+					lexer.StrToken{Str: "bar"},
+					lexer.BlockToken{Block: "}}"},
+					lexer.EOLToken{},
+				},
+			},
+			nodes: []TreeNode{
+				NonTerminalParseNode{},
+				StringParseNode{},
+				NonTerminalParseNode{},
+			},
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{"},
+					lexer.NumToken{Num: "5"},
+					lexer.MultOpToken{Operator: "*"},
+					lexer.NumToken{Num: "4"},
+					lexer.BlockToken{Block: "}}"},
+					lexer.EOLToken{},
+				},
+			},
+			nodes: []TreeNode{
+				NonTerminalParseNode{},
+			},
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{"},
+					lexer.UnaryOpToken{Operator: "!"},
+					lexer.BoolToken{},
+					lexer.BlockToken{Block: "}}"},
+					lexer.EOLToken{},
+				},
+			},
+			nodes: []TreeNode{
+				NonTerminalParseNode{},
+			},
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{:"},
+					lexer.VarToken{Variable: "post.title"},
+					lexer.BlockToken{Block: "}}"},
+					lexer.EOLToken{},
+				},
+			},
+			nodes: []TreeNode{
+				NonTerminalParseNode{},
+			},
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{:"},
+					lexer.IdentToken{Identifier: "bar"},
+					lexer.BlockToken{Block: "}}"},
+					lexer.EOLToken{},
+				},
+			},
+			nodes: []TreeNode{
+				NonTerminalParseNode{},
+			},
+		},
 	}
 
-	for _, test := range testing {
-		tokChan := make(chan []lexer.Token)
-
-		go func(tokens [][]lexer.Token) {
-			defer close(tokChan)
-
-			for _, tokenLine := range tokens {
-				tokChan <- tokenLine
-			}
-		}(test.tokens)
-
-		nodes := []TreeNode{}
+	for _, test := range tests {
+		tokChan := getTokChan(test.tokens)
 		nodeChan := make(chan TreeNode)
 		errChan := make(chan error)
 
 		go Parse(tokChan, nodeChan, errChan)
+
+		nodes := []TreeNode{}
 		for node := range nodeChan {
 			nodes = append(nodes, node)
 		}
@@ -194,6 +263,189 @@ func TestNonPassthroughTokensReturnCorrectNodeTypes(t *testing.T) {
 			t.Errorf("Expected %v. Got %v.", test.nodes, nodes)
 		}
 	}
+}
+
+func TestParserSendsErrorWithIncorrectToken(t *testing.T) {
+	var tests = []struct {
+		tokens [][]lexer.Token
+		errMsg string
+	}{
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{"},
+					lexer.BlockToken{Block: "{{"},
+				},
+			},
+			errMsg: `unexpected symbol "{{"`,
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{:"},
+					lexer.BlockToken{Block: "}}"},
+				},
+			},
+			errMsg: `unexpected symbol "}}"`,
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{"},
+					lexer.ForToken{},
+					lexer.MultOpToken{Operator: "*"},
+				},
+			},
+			errMsg: `unexpected symbol "MULT_OP"`,
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{"},
+					lexer.ForToken{},
+					lexer.MultOpToken{Operator: "/"},
+				},
+			},
+			errMsg: `unexpected symbol "MULT_OP"`,
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{"},
+					lexer.ForToken{},
+					lexer.MultOpToken{Operator: "%"},
+				},
+			},
+			errMsg: `unexpected symbol "MULT_OP"`,
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{"},
+					lexer.ForToken{},
+					lexer.AddOpToken{Operator: "+"},
+				},
+			},
+			errMsg: `unexpected symbol "ADD_OP"`,
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{"},
+					lexer.ForToken{},
+					lexer.AddOpToken{Operator: "-"},
+				},
+			},
+			errMsg: `unexpected symbol "ADD_OP"`,
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{"},
+					lexer.SymbolToken{Symbol: "^"},
+				},
+			},
+			errMsg: `unrecognized symbol "^"`,
+		},
+		{
+			tokens: [][]lexer.Token{
+				{
+					lexer.BlockToken{Block: "{{"},
+					lexer.BlockToken{Block: "}}"},
+				},
+				{
+					lexer.SymbolToken{Symbol: "?"},
+				},
+			},
+			errMsg: `unrecognized symbol "?"`,
+		},
+	}
+
+	for _, test := range tests {
+		tokChan := getTokChan(test.tokens)
+		nodeChan := make(chan TreeNode)
+		errChan := make(chan error)
+		go Parse(tokChan, nodeChan, errChan)
+		go func() { <-nodeChan }()
+
+		err := <-errChan
+
+		if err == nil {
+			t.Errorf("Expected error %q. Got nil.", test.errMsg)
+		}
+
+		gotMsg := err.Error()
+		if strings.Index(gotMsg, test.errMsg) == -1 {
+			t.Errorf("Expected error %q to include %q.", gotMsg, test.errMsg)
+		}
+	}
+}
+
+func TestParserBuildsCorrectTree(t *testing.T) {
+	var tests = []struct {
+		tokens [][]lexer.Token
+		nodes  []TreeNode
+	}{
+		{
+			tokens: [][]lexer.Token{{
+				lexer.BlockToken{Block: "{{"},
+				lexer.BlockToken{Block: "}}"},
+				lexer.EOLToken{},
+			}},
+			nodes: []TreeNode{
+				NonTerminalParseNode{},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		tokChan := getTokChan(test.tokens)
+		nodeChan := make(chan TreeNode)
+		errChan := make(chan error)
+
+		go Parse(tokChan, nodeChan, errChan)
+
+		nodes := []TreeNode{}
+
+		go func() {
+			for node := range nodeChan {
+				nodes = append(nodes, node)
+			}
+		}()
+
+		if err := <-errChan; err != nil {
+			t.Errorf("Expected no errors. Got %q.", err.Error())
+		}
+
+		flatChildren := flattenChildren(nodes[0])
+
+		if !nodeSlicesEqual(flatChildren, test.nodes) {
+			t.Errorf("Expected %#v. Got %#v.", test.nodes, nodes)
+		}
+	}
+}
+
+func getTokChan(tokens [][]lexer.Token) chan []lexer.Token {
+	tokChan := make(chan []lexer.Token)
+
+	go func(tokens [][]lexer.Token) {
+		defer close(tokChan)
+
+		for _, tokenLine := range tokens {
+			tokChan <- tokenLine
+		}
+	}(tokens)
+
+	return tokChan
+}
+
+func flattenChildren(node TreeNode) []TreeNode {
+	allChildren := node.GetChildren()
+	for _, child := range allChildren {
+		allChildren = append(allChildren, flattenChildren(child)...)
+	}
+
+	return allChildren
 }
 
 func nodeSlicesEqual(expected, got []TreeNode) bool {
