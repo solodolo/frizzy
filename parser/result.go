7476f1ff1bc7da9b122a883bb0267b903a96baf3
Drew Mettlach <dmmettlach@gmail.com>
2021-02-06 03:57:10 +0000

    Add LogicalResult and SubtractableResult interfaces

--- parser/result.go
+++ parser/result.go
@@ -8,6 +8,19 @@ type AddableResult interface {
 	Add(right Result) (Result, error)
 }
 
+type SubtractableResult interface {
+	Subtract(right Result) (Result, error)
+}
+
 type MultipliableResult interface {
 	Multiply(right Result) (Result, error)
+	Divide(right Result) (Result, error)
+}
+
+type LogicalResult interface {
+	LessThan(right Result) (Result, error)
+	GreaterThan(right Result) (Result, error)
+	EqualTo(right Result) (Result, error)
+	LessThanEqual(right Result) (Result, error)
+	GreaterThanEqual(right Result) (Result, error)
 }
