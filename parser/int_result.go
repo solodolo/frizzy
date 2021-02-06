01e574029bebf92cf6d881c855379da0a547c31f
Drew Mettlach <dmmettlach@gmail.com>
2021-02-06 03:58:22 +0000

    Add subtract implementation and move common functionality out of logic implementation methods

--- parser/int_result.go
+++ parser/int_result.go
@@ -31,6 +31,18 @@ func (receiver IntResult) Add(right Result) (Result, error) {
 	}
 }
 
+// Subtract takes a result and subtracts it from this integer representation
+// It returns a Result type or an error if right cannot be subtracted
+// to receiver
+func (receiver IntResult) Subtract(right Result) (Result, error) {
+	switch typedRight := right.(type) {
+	case IntResult:
+		return IntResult(receiver - typedRight), nil
+	default:
+		return nil, fmt.Errorf("Cannt subtract %T and %T", receiver, right)
+	}
+}
+
 // Multiply takes a result and multiplies it with this integer representation
 // Returns a Result type or an error if right cannot be multiplied with receiver
 func (receiver IntResult) Multiply(right Result) (Result, error) {
@@ -54,82 +66,61 @@ func (receiver IntResult) Divide(right Result) (Result, error) {
 	}
 }
 
+func convertToInt(right Result) (int, bool) {
+	switch typedResult := right.(type) {
+	case IntResult:
+		return int(typedResult), true
+	case StringResult:
+		if num, err := strconv.Atoi(string(typedResult)); err == nil {
+			return num, true
+		}
+		return 0, false
+	default:
+		return 0, false
+	}
+}
+
 // LessThan checks if the provided result is logically less than
 // the receiver
 func (receiver IntResult) LessThan(right Result) (Result, error) {
-	switch typedResult := right.(type) {
-	case IntResult:
-		return BoolResult(receiver < typedResult), nil
-	case StringResult:
-		if num, err := strconv.Atoi(string(typedResult)); err == nil {
-			return BoolResult(int(receiver) < num), nil
-		}
-		return nil, fmt.Errorf("Invalid comparison %v < %v", receiver, right)
-	default:
-		return nil, fmt.Errorf("Cannot determine %T < %T", receiver, right)
+	if rightInt, ok := convertToInt(right); ok {
+		return BoolResult(int(receiver) < rightInt), nil
 	}
+	return nil, fmt.Errorf("Cannot determine %T < %T", receiver, right)
 }
 
 // GreaterThan checks if the provided result is logically greater than
 // the receiver
 func (receiver IntResult) GreaterThan(right Result) (Result, error) {
-	switch typedResult := right.(type) {
-	case IntResult:
-		return BoolResult(receiver > typedResult), nil
-	case StringResult:
-		if num, err := strconv.Atoi(string(typedResult)); err == nil {
-			return BoolResult(int(receiver) > num), nil
-		}
-		return nil, fmt.Errorf("Invalid comparison %v > %v", receiver, right)
-	default:
-		return nil, fmt.Errorf("Cannot determine %T > %T", receiver, right)
+	if rightInt, ok := convertToInt(right); ok {
+		return BoolResult(int(receiver) > rightInt), nil
 	}
+	return nil, fmt.Errorf("Cannot determine %T > %T", receiver, right)
 }
 
 // EqualTo checks if the provided result is logically equal to
 // the receiver
 func (receiver IntResult) EqualTo(right Result) (Result, error) {
-	switch typedResult := right.(type) {
-	case IntResult:
-		return BoolResult(receiver == typedResult), nil
-	case StringResult:
-		if num, err := strconv.Atoi(string(typedResult)); err == nil {
-			return BoolResult(int(receiver) == num), nil
-		}
-		return nil, fmt.Errorf("Invalid comparison %v == %v", receiver, right)
-	default:
-		return nil, fmt.Errorf("Cannot determine %T == %T", receiver, right)
+	if rightInt, ok := convertToInt(right); ok {
+		return BoolResult(int(receiver) == rightInt), nil
 	}
+	return nil, fmt.Errorf("Cannot determine %T == %T", receiver, right)
 }
 
 // LessThanEqual checks if the provided result is logically less than or equal to
 // the receiver
 func (receiver IntResult) LessThanEqual(right Result) (Result, error) {
-	switch typedResult := right.(type) {
-	case IntResult:
-		return BoolResult(receiver <= typedResult), nil
-	case StringResult:
-		if num, err := strconv.Atoi(string(typedResult)); err == nil {
-			return BoolResult(int(receiver) <= num), nil
-		}
-		return nil, fmt.Errorf("Invalid comparison %v <= %v", receiver, right)
-	default:
-		return nil, fmt.Errorf("Cannot determine %T <= %T", receiver, right)
+	if rightInt, ok := convertToInt(right); ok {
+		return BoolResult(int(receiver) <= rightInt), nil
 	}
+	return nil, fmt.Errorf("Cannot determine %T <= %T", receiver, right)
 }
 
 // GreaterThanEqual checks if the provided result is logically greater than or equal to
 // the receiver
 func (receiver IntResult) GreaterThanEqual(right Result) (Result, error) {
-	switch typedResult := right.(type) {
-	case IntResult:
-		return BoolResult(receiver >= typedResult), nil
-	case StringResult:
-		if num, err := strconv.Atoi(string(typedResult)); err == nil {
-			return BoolResult(int(receiver) >= num), nil
-		}
-		return nil, fmt.Errorf("Invalid comparison %v >= %v", receiver, right)
-	default:
-		return nil, fmt.Errorf("Cannot determine %T >= %T", receiver, right)
+	if rightInt, ok := convertToInt(right); ok {
+		return BoolResult(int(receiver) >= rightInt), nil
 	}
+	return nil, fmt.Errorf("Cannot determine %T >= %T", receiver, right)
 }
