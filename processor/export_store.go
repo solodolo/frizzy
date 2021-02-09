db4884e40edcb5e3ad96c78edb664ae7fe13037f
Drew Mettlach <dmmettlach@gmail.com>
2021-02-09 04:11:41 +0000

    Make export store concurrent safe

--- processor/export_store.go
+++ processor/export_store.go
@@ -1,33 +1,43 @@
+package processor
+
+import (
+	"sync"
+)
+
 // Files can export variables for use by other files
 // All assignment variables are exported
 
-// exportStore is a singleton to read and write export vars
-type exportStore struct {
+// ExportStore is a singleton to read and write export vars
+type ExportStore struct {
 	exports map[string]Context
 }
 
-var store *exportStore
+var once sync.Once
+var store *ExportStore
+
+func createStore() {
+	if store == nil {
+		store = &ExportStore{}
+	}
+}
 
 // GetExportStore returns the exportStore singleton
-func GetExportStore() *exportStore {
-	if store == nil {
-		store = &exportStore{}
-	}
-
+func GetExportStore() *ExportStore {
+	once.Do(createStore)
 	return store
 }
 
-// Inserts inserts the key, value pair of the export context
+// Insert inserts the key, value pair of the export context
 // represented by filename
-func (receiver *exportStore) Insert(filename, key string, value Result) {
-	if ctx, ok := receiver.exports[filename]; ok {
+func (receiver *ExportStore) Insert(filename, key string, value Result) {
+	if _, ok := receiver.exports[filename]; ok {
 		receiver.exports[filename][key] = value
 	} else {
 		receiver.exports[filename] = Context{key: value}
 	}
 }
 
-// ContextFor returns the export context of the given filename
-func (receiver *exportStore) ContextFor(filename string) Context {
+// Get returns the export context of the given filename
+func (receiver *ExportStore) Get(filename string) Context {
 	return receiver.exports[filename]
 }
