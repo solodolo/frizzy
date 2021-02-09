2aadbbdb16757484359ee99687b5ac586c3ca4de
Drew Mettlach <dmmettlach@gmail.com>
2021-02-09 04:09:20 +0000

    Add merge method to Context type

--- processor/context.go
+++ processor/context.go
@@ -3,3 +3,11 @@ package processor
 // Context is the set of methods avaialble for storing parsing
 // context data
 type Context map[string]Result
+
+// Merge adds the keys and values from other into receiver
+// Matching keys in receiver will be overwritten
+func (receiver *Context) Merge(other *Context) {
+	for k, v := range *other {
+		(*receiver)[k] = v
+	}
+}
