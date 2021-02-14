f037b3e63900cbc2b9f7c2edfb7de52f91aa9ba9
Drew Mettlach <dmmettlach@gmail.com>
2021-02-14 23:47:49 +0000

    Add method to get array of arguments from parent arguments node

--- parser/args_parse_node.go
+++ parser/args_parse_node.go
@@ -9,3 +9,15 @@ type ArgsParseNode struct {
 func (receiver *ArgsParseNode) String() string {
 	return fmt.Sprintf("%T", *receiver)
 }
+
+// GetArguments returns an array of nodes representing the arguments
+// from left to right
+// Return will be empty if no arguments are present
+func (receiver *ArgsParseNode) GetArguments() []TreeNode {
+	if len(receiver.children) > 0 {
+		argsList := receiver.children[0].(*ArgsListParseNode)
+		return argsList.GetArguments()
+	}
+
+	return []TreeNode{}
+}
