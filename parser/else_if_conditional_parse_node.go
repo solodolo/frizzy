24725e64d6554d60c0f0c51b171a1fd85f39e407
Drew Mettlach <dmmettlach@gmail.com>
2021-02-14 04:39:43 +0000

    Add method to get else_if by index

--- parser/else_if_conditional_parse_node.go
+++ parser/else_if_conditional_parse_node.go
@@ -14,8 +14,32 @@ func (receiver *ElseIfConditionalParseNode) String() string {
 	return fmt.Sprintf("%T", *receiver)
 }
 
+func (receiver *ElseIfConditionalParseNode) getElseIfList() (*ElseIfListParseNode, bool) {
+	if len(receiver.children) > 0 {
+		return receiver.children[0].(*ElseIfListParseNode), true
+	}
+	return nil, false
+}
+
 // GetConditionals simply wraps ElseIfListParseNode GetConditionals
 func (receiver *ElseIfConditionalParseNode) GetConditionals() []TreeNode {
-	child := receiver.children[0].(*ElseIfListParseNode)
-	return child.GetConditionals()
+	conditionals := []TreeNode{}
+	if elseIf, ok := receiver.getElseIfList(); ok {
+		conditionals = elseIf.GetConditionals()
+	}
+	return conditionals
+}
+
+// GetElseIfAt returns the nth child else_if where n = index
+// If index is out of bounds or else_if isn't found
+// the second return value will be false
+func (receiver *ElseIfConditionalParseNode) GetElseIfAt(index int) (TreeNode, bool) {
+	if elseIf, ok := receiver.getElseIfList(); ok {
+		flat := elseIf.GetFlattenedBlockChildren()
+		if len(flat) > index {
+			return flat[index], ok
+		}
+	}
+
+	return nil, false
 }
