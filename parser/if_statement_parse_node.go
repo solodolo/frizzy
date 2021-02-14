15dfded76e099b59c1f2ca4e9279dd7deff3a0ee
Drew Mettlach <dmmettlach@gmail.com>
2021-02-14 04:41:49 +0000

    Add methods to parent if statement to get if and else_if conditionals and bodies
    these methods will be the interface for the processor to process if statements

--- parser/if_statement_parse_node.go
+++ parser/if_statement_parse_node.go
@@ -6,17 +6,63 @@ import "fmt"
 // if statement
 // The children are the elemnts of the if statment including
 // each if/else_if/else block
-//
+// |  |-- parser.IfStatementParseNode
+// |     |-- parser.IfConditionalParseNode
+// |     |-- parser.ElseIfConditionalParseNode
+// |     |  |-- parser.ElseIfListParseNode
+// |     |     |-- parser.ElseIfListParseNode...
+// |     |-- parser.ElseParseNode
+// |     |-- parser.IdentParseNode: end
 type IfStatementParseNode struct {
 	ParseNode
 }
 
-func (receiver IfStatementParseNode) String() string {
-	return fmt.Sprintf("%T", receiver)
+func (receiver *IfStatementParseNode) String() string {
+	return fmt.Sprintf("%T", *receiver)
 }
 
-// GetSelectedBody examines each block of this if statement in order
-// and returns the TreeNodes in the body of the first true block
-func (receiver IfStatementParseNode) GetSelectedBody() []TreeNode {
-	return nil
+func (receiver *IfStatementParseNode) getIfConditional() *IfConditionalParseNode {
+	return receiver.children[0].(*IfConditionalParseNode)
+}
+
+// GetIfConditional returns the child if statement conditional
+func (receiver *IfStatementParseNode) GetIfConditional() BoolParseNode {
+	ifConditional := receiver.getIfConditional()
+	return *ifConditional.GetConditional().(*BoolParseNode)
+}
+
+// GetIfConditionalBody returns the body of this node's if statement
+func (receiver *IfStatementParseNode) GetIfConditionalBody() TreeNode {
+	ifConditional := receiver.getIfConditional()
+	return ifConditional.GetBody()
+}
+
+func (receiver *IfStatementParseNode) getElseIfConditional() (*ElseIfConditionalParseNode, bool) {
+	elseIf, ok := receiver.children[1].(*ElseIfConditionalParseNode)
+	return elseIf, ok
+}
+
+// GetElseIfConditionals returns an array of conditional nodes from each
+// of this node's else_if children
+// Return value may be empty
+func (receiver *IfStatementParseNode) GetElseIfConditionals() []BoolParseNode {
+	conditionals := []BoolParseNode{}
+
+	if elseIfConditional, ok := receiver.getElseIfConditional(); ok {
+		for _, conditional := range elseIfConditional.GetConditionals() {
+			conditionals = append(conditionals, *conditional.(*BoolParseNode))
+		}
+	}
+	return conditionals
+}
+
+// GetElseIfBody returns the body of the index'th else_if or false if not found
+func (receiver *IfStatementParseNode) GetElseIfBody(index int) (TreeNode, bool) {
+	if elseIfConditional, ok := receiver.getElseIfConditional(); ok {
+		if elseIfAt, ok := elseIfConditional.GetElseIfAt(index); ok {
+			return elseIfAt.(*ElseIfListParseNode).GetBody(), true
+		}
+	}
+
+	return nil, false
 }
