c632c5116e3b114315d26ceaf9902ef462d7a35a
Drew Mettlach <dmmettlach@gmail.com>
2021-02-14 23:44:16 +0000

    Add methods to get array of arguments from nested arg list nodes

--- parser/args_list_parse_node.go
+++ parser/args_list_parse_node.go
@@ -2,6 +2,7 @@ package parser
 
 import "fmt"
 
+// ArgsListParseNode represents a comma separated list of function arguments
 type ArgsListParseNode struct {
 	ParseNode
 	flattenedChildren []*ArgsListParseNode
@@ -9,6 +10,19 @@ type ArgsListParseNode struct {
 
 func (receiver *ArgsListParseNode) String() string {
 	return fmt.Sprintf("%T", *receiver)
+}
+
+// GetArguments returns an array of nodes representing the arguments
+// from left to right
+func (receiver *ArgsListParseNode) GetArguments() []TreeNode {
+	flat := receiver.GetFlattenedChildren()
+	args := make([]TreeNode, len(flat))
+
+	for i, child := range flat {
+		args[i] = child.getArg()
+	}
+
+	return args
 }
 
 // GetFlattenedChildren returns the ArgsListParseNode children of
@@ -41,4 +55,11 @@ func (receiver *ArgsListParseNode) hasNestedChildren() bool {
 	}
 	_, ok := receiver.children[0].(*ArgsListParseNode)
 	return ok
+}
+
+func (receiver *ArgsListParseNode) getArg() TreeNode {
+	if receiver.hasNestedChildren() {
+		return receiver.children[2]
+	}
+	return receiver.children[0]
 }
