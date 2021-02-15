550d82d8f4c8d9eb2763429df4f3e3a661e719f3
Drew Mettlach <dmmettlach@gmail.com>
2021-02-15 00:06:39 +0000

    Add methods to get name and arguments of func call node

--- parser/func_call_parse_node.go
+++ parser/func_call_parse_node.go
@@ -9,6 +9,24 @@ type FuncCallParseNode struct {
 	ParseNode
 }
 
-func (receiver FuncCallParseNode) String() string {
-	return fmt.Sprintf("%T", receiver)
+func (receiver *FuncCallParseNode) String() string {
+	return fmt.Sprintf("%T", *receiver)
+}
+
+// GetFuncName returns the name of the function that
+// this node represents
+func (receiver *FuncCallParseNode) GetFuncName() string {
+	nameNode := receiver.children[0].(*IdentParseNode)
+	return nameNode.Value
+}
+
+func (receiver *FuncCallParseNode) getArgNode() *ArgsParseNode {
+	return receiver.children[2].(*ArgsParseNode)
+}
+
+// GetArgs returns a slice of nodes representing the arguments
+// to this function
+func (receiver *FuncCallParseNode) GetArgs() []TreeNode {
+	argNode := receiver.getArgNode()
+	return argNode.GetArguments()
 }
