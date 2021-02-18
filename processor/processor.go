d131125625812bc86ececda97cfbdc2e5d8222b1
Drew Mettlach <dmmettlach@gmail.com>
2021-02-18 22:46:52 +0000

    Add missing parser section for relation nodes

--- processor/processor.go
+++ processor/processor.go
@@ -47,6 +47,13 @@ func processHeadNode(head parser.TreeNode, context *Context) Result {
 				panic(fmt.Sprintf("multiplication error: %s", err))
 			}
 			return multResult
+		} else if typedNode.IsRelation() {
+			relResult, err := processRel(getBinaryOperatorAndOperands(children, context))
+
+			if err != nil {
+				panic(fmt.Sprintf("rel error: %s", err))
+			}
+			return relResult
 		} else if typedNode.IsLogic() {
 			logicResult, err := processLogic(getBinaryOperatorAndOperands(children, context))
 
@@ -242,23 +249,40 @@ func processMultiplication(left, right Result, operator string) (Result, error) 
 
 func processLogic(left, right Result, operator string) (Result, error) {
 	leftOp := left.(LogicalResult)
-	if operator == "<" {
-		return leftOp.LessThan(right)
-	} else if operator == ">" {
-		return leftOp.GreaterThan(right)
-	} else if operator == "==" {
-		return leftOp.EqualTo(right)
-	} else if operator == "<=" {
-		return leftOp.LessThanEqual(right)
-	} else if operator == ">=" {
-		return leftOp.GreaterThanEqual(right)
+	if operator == "&&" {
+		return leftOp.LogicalAnd(right)
+	} else if operator == "||" {
+		return leftOp.LogicalOr(right)
 	}
 
-	return nil, fmt.Errorf("Invalid logic operator %q", operator)
+	return nil, fmt.Errorf("Invalid logcal operator %q", operator)
+}
+
+// TODO: Figure out a better way to look up the appropriate functions
+func processRel(left, right Result, operator string) (Result, error) {
+	if operator == "==" {
+		leftOp := left.(EqualityResult)
+		return leftOp.EqualTo(right)
+	} else if operator == "!=" {
+		leftOp := left.(EqualityResult)
+		return leftOp.NotEqualTo(right)
+	} else {
+		leftOp := left.(RelResult)
+		if operator == "<" {
+			return leftOp.LessThan(right)
+		} else if operator == ">" {
+			return leftOp.GreaterThan(right)
+		} else if operator == "<=" {
+			return leftOp.LessThanEqual(right)
+		} else if operator == ">=" {
+			return leftOp.GreaterThanEqual(right)
+		}
+	}
+
+	return nil, fmt.Errorf("Invalid relation operator %q", operator)
 }
 
 func processUnary(right Result, operator string) (Result, error) {
-
 	if operator == "!" {
 		rightOp := right.(NotResult)
 		return rightOp.Not()
