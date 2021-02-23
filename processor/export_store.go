c30d37ece5950d00048500a01f35b7a233a7f078
Drew Mettlach <dmmettlach@gmail.com>
2021-02-23 04:34:06 +0000

    Add light wrapper around export store to perform inserts for a specific file

--- processor/export_store.go
+++ processor/export_store.go
@@ -7,6 +7,40 @@ import (
 // Files can export variables for use by other files
 // All assignment variables are exported
 
+// ExportStorage represents a type that can store results
+// into a context and return that context
+type ExportStorage interface {
+	Insert(string, Result)
+	GetContext() *Context
+	GetFileContext(string) *Context
+}
+
+// ExportFileStore is a wrapper around ExportStore for a
+// specific file
+type ExportFileStore struct {
+	filePath string
+}
+
+// Insert inserts the key, value pair of the export context
+// represented by filePath
+func (receiver *ExportFileStore) Insert(contextKey string, value Result) {
+	exportStore := GetExportStore()
+	exportStore.Insert(receiver.filePath, contextKey, value)
+}
+
+// GetContext returns the export context associated with this
+// ExportFileStore filePath
+func (receiver *ExportFileStore) GetContext() *Context {
+	exportStore := GetExportStore()
+	return exportStore.Get(receiver.filePath)
+}
+
+// GetFileContext returns the context associated with the given filePath
+func (receiver *ExportFileStore) GetFileContext(filePath string) *Context {
+	exportStore := GetExportStore()
+	return exportStore.Get(filePath)
+}
+
 // ExportStore is a singleton to read and write export vars
 type ExportStore struct {
 	exports map[string]*Context
@@ -17,7 +51,7 @@ var store *ExportStore
 
 func createStore() {
 	if store == nil {
-		store = &ExportStore{}
+		store = &ExportStore{exports: make(map[string]*Context)}
 	}
 }
 
@@ -27,15 +61,20 @@ func GetExportStore() *ExportStore {
 	return store
 }
 
+var mut sync.Mutex
+
 // Insert inserts the key, value pair of the export context
 // represented by filename
-func (receiver *ExportStore) Insert(filename, key string, value Result) {
+func (receiver *ExportStore) Insert(filename, contextKey string, value Result) {
+	mut.Lock()
+	defer mut.Unlock()
+
 	if _, ok := receiver.exports[filename]; ok {
 		context := receiver.exports[filename]
-		(*context)[key] = ContextNode{result: value}
+		(*context)[contextKey] = ContextNode{result: value}
 	} else {
 		val := ContextNode{result: value}
-		receiver.exports[filename] = &Context{key: val}
+		receiver.exports[filename] = &Context{contextKey: val}
 	}
 }
 
