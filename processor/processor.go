268e335c9cddb3f95b55228b71ba715f68d7a295
Drew Mettlach <dmmettlach@gmail.com>
2021-02-23 18:45:41 +0000

    Remove unused comments

--- processor/processor.go
+++ processor/processor.go
@@ -194,34 +194,7 @@ func (receiver *NodeProcessor) processHeadNode(head parser.TreeNode) Result {
 	default:
 		return StringResult("")
 	}
-	//
-	// send context down recursively to each child
-	//
-	// if head doesn't have any children (or if a NumNode, StrNode ?)
-	//	if head is an int value
-	//		return IntResult
-	//	else if head is a string value
-	//		return StringResult
-	//	else if head is a bool value
-	//		return BoolResult
-	// else if binary operator node
-	//	perform operation with left and right Result
-	// else if unary operator node
-	//	perform operation with left Result
-	// else if func call node
-	//	left node is func name and the rest are arguments
-	// else if an else if node
-	//	if statement is true then return body value
-	//	else return nil
-	// else if an else node
-	//	return body value
-	// else if an if node
-	//	if statement is true return if body
-	//	else return first child body that isn't nil
-	// else if a for loop node (want to add to context for children so 1. determine number of loops 2. determine context for each loop 3. traverse children after statement with each context)
-	// else print block or non-print block
-	//	what do we do here?
-	//
+
 	return nil
 }
 
