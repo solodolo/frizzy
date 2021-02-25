ca6431ca7f5b9e162d35a8f23f24e5fdadacbde3
Drew Mettlach <dmmettlach@gmail.com>
2021-02-25 06:02:23 +0000

    Add missing comments to exported function module functions

--- processor/function_module.go
+++ processor/function_module.go
@@ -7,12 +7,16 @@ type FunctionModule interface {
 	registerFunc(string, func(...Result) Result)
 }
 
+// BuiltinFunctionModule acts as an interface between callers
+// and builtin Frizzy functions
 type BuiltinFunctionModule map[string]func(...Result) Result
 
 func (receiver *BuiltinFunctionModule) registerFunc(funcName string, function func(...Result) Result) {
 	(*receiver)[funcName] = function
 }
 
+// CallFunction calls the function with the given name and passes in
+// the provided arguments
 func (receiver *BuiltinFunctionModule) CallFunction(funcName string, funcArgs ...Result) (Result, bool) {
 	if function, ok := (*receiver)[funcName]; ok {
 		return function(funcArgs...), true
@@ -21,6 +25,8 @@ func (receiver *BuiltinFunctionModule) CallFunction(funcName string, funcArgs ..
 	return nil, false
 }
 
+// NewBuiltinFunctionModule creates a BuiltinFunctionModule object
+// populated with a mapping for each of the builtin functions
 func NewBuiltinFunctionModule() BuiltinFunctionModule {
 	module := BuiltinFunctionModule{}
 	module.registerFunc("print", PrintRaw)
