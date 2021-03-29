b2e3d94a4e9d00096dbb4db6f8bf2ba65bcb2bb7
Drew Mettlach <drew@mettlach.codes>
2021-03-29 03:56:08 +0000

    Move logic for creating output files into separate method

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -17,57 +16,61 @@ func main() {
 	if config, err := config.LoadConfig("/home/dmmettlach/workspace/frizzy/config.json"); err != nil {
 		log.Fatal(fmt.Errorf("error loading config: %s", err))
 	} else {
-		// if err := unix.Chroot(config.RootPath); err != nil {
-		// 	log.Fatalf("chroot error: %s", err)
-		// }
-		contentPath := config.ContentPath
-
-		filepath.Walk(contentPath, func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				log.Fatalf("error walking content dir: %s", err)
-			}
-			// ignore nested dirs for now
-			if info.IsDir() {
-				return nil
-			}
-
-			if contentFile, err := os.Open(path); err != nil {
-				log.Fatalf("could not open content file %s for processing", path)
-			} else {
-				log.Default().Printf("processing %s\n", path)
-				tokChan := make(chan []lexer.Token)
-				errChan := make(chan error)
-
-				nodeChan := make(chan parser.TreeNode)
-				parserErrChan := make(chan error)
-
-				nodeProcessor := processor.NodeProcessor{
-					Context: &processor.Context{},
-				}
-
-				resultChan := make(chan processor.Result)
-				go lexer.Lex(bufio.NewScanner(contentFile), tokChan, errChan)
-				go parser.Parse(tokChan, nodeChan, parserErrChan)
-				go nodeProcessor.Process(nodeChan, resultChan)
-
-				for result := range resultChan {
-					fmt.Println(result)
-				}
-
-				select {
-				case tokErr := <-errChan:
-					if tokErr != nil {
-						log.Fatalf("lexer error: %s", tokErr)
-					}
-				case parseErr := <-parserErrChan:
-					if parseErr != nil {
-						log.Fatalf("parser error: %s", parseErr)
-					}
-				}
-			}
-			return nil
-		})
+		processContent(config.ContentPath)
+		processContent(config.RootPath)
 
 		fmt.Println("Done")
+	}
+}
+
+func processContent(path string) {
+	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		fmt.Println("path: " + path)
+		if err != nil {
+			log.Fatalf("error walking dir: %s", err)
+		}
+		// ignore nested dirs for now
+		if info.IsDir() {
+			return nil
+		}
+
+		if file, err := os.Open(path); err != nil {
+			log.Fatalf("could not open file %s for processing", path)
+		} else {
+			log.Default().Printf("processing %s\n", path)
+			renderFile(file)
+		}
+		return nil
+	})
+}
+
+func renderFile(contentFile *os.File) {
+	nodeChan := make(chan parser.TreeNode)
+	parserErrChan := make(chan error)
+	exportStore := processor.NewExportFileStore(contentFile.Name())
+	nodeProcessor := processor.NodeProcessor{
+		Context:     &processor.Context{},
+		ExportStore: exportStore,
+	}
+
+	resultChan := make(chan processor.Result)
+	lexer := lexer.Lexer{}
+	tokChan, lexErrChan := lexer.Lex(contentFile)
+	go parser.Parse(tokChan, nodeChan, parserErrChan)
+	go nodeProcessor.Process(nodeChan, resultChan)
+
+	for result := range resultChan {
+		fmt.Print(result)
+	}
+
+	select {
+	case tokErr := <-lexErrChan:
+		if tokErr != nil {
+			log.Fatalf("lexer error: %s", tokErr)
+		}
+	case parseErr := <-parserErrChan:
+		if parseErr != nil {
+			log.Fatalf("parser error: %s", parseErr)
+		}
 	}
 }
