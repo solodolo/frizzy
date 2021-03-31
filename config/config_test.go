df5c270bdbfb3df2f9b20eec2f81e2e90831cafb
Drew Mettlach <drew@mettlach.codes>
2021-03-31 03:36:06 +0000

    Test default config settings

--- config/config_test.go
+++ config/config_test.go
@@ -2,20 +2,63 @@ package config
 
 import (
 	"fmt"
+	"path/filepath"
 	"strings"
 	"testing"
 )
 
-func TestConfigLoadsCorrectValues(t *testing.T) {
-	expectedRoot := "/path/to/project/root"
-	expectedContent := "path/to/content"
+var expectedRoot = "/path/to/project/root"
 
-	configJSON := fmt.Sprintf(`{"RootPath": %q, "ContentPath": %q}`, expectedRoot, expectedContent)
+func TestConfigSetsDefaultContentDir(t *testing.T) {
+	expectedContent := filepath.Join(expectedRoot, "content")
+
+	configJSON := fmt.Sprintf(`{"RootPath": %q}`, expectedRoot)
 	if config, err := loadConfigObject(strings.NewReader(configJSON)); err != nil {
 		t.Errorf("expected no error, got %q", err)
 	} else if config.RootPath != expectedRoot {
 		t.Errorf(`expected RootPath to be %q, got %q`, expectedRoot, config.RootPath)
-	} else if config.ContentPath != expectedContent {
-		t.Errorf(`expected ContentPath to be %q got %q`, expectedContent, config.ContentPath)
+	} else if config.GetContentPath() != expectedContent {
+		t.Errorf(`expected content path to be %q got %q`, expectedContent, config.GetContentPath())
+	}
+}
+
+func TestConfigSetsDefaultPageDir(t *testing.T) {
+	expectedPages := filepath.Join(expectedRoot, "pages")
+
+	configJSON := fmt.Sprintf(`{"RootPath": %q}`, expectedRoot)
+	if config, err := loadConfigObject(strings.NewReader(configJSON)); err != nil {
+		t.Errorf("expected no error, got %q", err)
+	} else if config.RootPath != expectedRoot {
+		t.Errorf(`expected RootPath to be %q, got %q`, expectedRoot, config.RootPath)
+	} else if config.GetPagesPath() != expectedPages {
+		t.Errorf(`expected pages path to be %q got %q`, expectedPages, config.GetContentPath())
+	}
+}
+
+func TestConfigSetsCorrectContentDir(t *testing.T) {
+	dir := "/some/content/dir"
+	expected := filepath.Join(expectedRoot, dir)
+
+	configJSON := fmt.Sprintf(`{"RootPath": %q, "ContentDir": %q}`, expectedRoot, dir)
+	if config, err := loadConfigObject(strings.NewReader(configJSON)); err != nil {
+		t.Errorf("expected no error, got %q", err)
+	} else if config.RootPath != expectedRoot {
+		t.Errorf(`expected RootPath to be %q, got %q`, expectedRoot, config.RootPath)
+	} else if config.GetContentPath() != expected {
+		t.Errorf(`expected content path to be %q got %q`, expected, config.GetContentPath())
+	}
+}
+
+func TestConfigSetsCorrectPagesDir(t *testing.T) {
+	dir := "/some/pages/dir"
+	expected := filepath.Join(expectedRoot, dir)
+
+	configJSON := fmt.Sprintf(`{"RootPath": %q, "PagesDir": %q}`, expectedRoot, dir)
+	if config, err := loadConfigObject(strings.NewReader(configJSON)); err != nil {
+		t.Errorf("expected no error, got %q", err)
+	} else if config.RootPath != expectedRoot {
+		t.Errorf(`expected RootPath to be %q, got %q`, expectedRoot, config.RootPath)
+	} else if config.GetPagesPath() != expected {
+		t.Errorf(`expected content path to be %q got %q`, expected, config.GetContentPath())
 	}
 }
