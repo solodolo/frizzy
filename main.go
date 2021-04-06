78a72f918c49340782802d636ab4f424e7c118c2
Drew Mettlach <drew@mettlach.codes>
2021-04-06 03:47:59 +0000

    Add method to write output to defined output directory

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 
 	"mettlach.codes/frizzy/config"
+	"mettlach.codes/frizzy/file"
 	"mettlach.codes/frizzy/lexer"
 	"mettlach.codes/frizzy/parser"
 	"mettlach.codes/frizzy/processor"
@@ -16,15 +17,23 @@ func main() {
 	if config, err := config.LoadConfig("/home/dmmettlach/workspace/frizzy/config.json"); err != nil {
 		log.Fatal(fmt.Errorf("error loading config: %s", err))
 	} else {
-		processContent(config.GetContentPath())
-		processContent(config.GetPagesPath())
+		outputPath := config.OutputPath
+		processContent(config.GetContentPath(), outputPath)
+		processContent(config.GetPagesPath(), outputPath)
 
 		fmt.Println("Done")
 	}
 }
 
-func processContent(path string) {
-	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+func createOutputDirs(outputPath string) {
+	if err := os.MkdirAll(outputPath, 0750); err != nil {
+		log.Fatalf("error creating output dir %q: %s\n", outputPath, err)
+	}
+
+}
+
+func processContent(inputPath, outputPath string) {
+	filepath.Walk(inputPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			log.Fatalf("error walking dir: %s", err)
 		}
@@ -37,13 +46,13 @@ func processContent(path string) {
 			log.Fatalf("could not open file %s for processing", path)
 		} else {
 			log.Default().Printf("processing %s\n", path)
-			renderFile(file)
+			renderFile(file, outputPath)
 		}
 		return nil
 	})
 }
 
-func renderFile(contentFile *os.File) {
+func renderFile(contentFile *os.File, outputPath string) {
 	nodeChan := make(chan parser.TreeNode)
 	parserErrChan := make(chan error)
 
@@ -56,7 +65,7 @@ func renderFile(contentFile *os.File) {
 	go nodeProcessor.Process(nodeChan, resultChan)
 
 	for result := range resultChan {
-		fmt.Print(result)
+		renderHTMLResult(result, contentFile.Name(), outputPath)
 	}
 
 	select {
@@ -68,5 +77,21 @@ func renderFile(contentFile *os.File) {
 		if parseErr != nil {
 			log.Fatalf("parser error: %s", parseErr)
 		}
+	}
+}
+
+func renderHTMLResult(result processor.Result, inputPath, outputPath string) {
+	relativeInputPath := file.GetRelativePathTo(inputPath)
+	fullPath := filepath.Join(outputPath, relativeInputPath)
+
+	if err := os.MkdirAll(filepath.Dir(fullPath), 0750); err != nil {
+		log.Fatalf("could not create output dir %q: %s\n", filepath.Dir(fullPath), err)
+	}
+
+	if f, err := os.Create(fullPath); err != nil {
+		fmt.Printf("could not create output file %q: %s\n", fullPath, err)
+	} else {
+		f.WriteString(result.String())
+		f.Close()
 	}
 }
