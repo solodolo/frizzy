46360b8a21473663fee6a168a089880f69cd6dff
Drew Mettlach <drew@mettlach.codes>
2021-04-07 03:38:52 +0000

    Add simple http dev server to serve files from output directory

---
+++ file/dev_server.go
@@ -0,0 +1,49 @@
+package file
+
+import (
+	"fmt"
+	"io"
+	"io/fs"
+	"net/http"
+	"os"
+	"time"
+)
+
+type DevServer struct {
+	ServerRoot string
+	Port       int
+}
+
+func (receiver *DevServer) ListenAndServe() error {
+	rootFS := os.DirFS(receiver.ServerRoot)
+	address := fmt.Sprintf(":%d", receiver.Port)
+	server := http.Server{
+		Addr:        address,
+		ReadTimeout: time.Second * 5,
+		Handler:     &devServerHandler{rootFS},
+	}
+
+	fmt.Printf("serving files from %s\n", receiver.ServerRoot)
+	fmt.Printf("listening on http://localhost:%d\n", receiver.Port)
+
+	return server.ListenAndServe()
+}
+
+type devServerHandler struct {
+	rootFS fs.FS
+}
+
+func (receiver *devServerHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	requestPath := request.URL.EscapedPath()
+	if f, err := receiver.rootFS.Open(requestPath[1:]); err != nil {
+		writer.WriteHeader(404)
+		io.WriteString(writer, fmt.Sprintf("File %s not found: %s", requestPath, err))
+	} else {
+		if bytes, err := io.ReadAll(f); err != nil {
+			writer.WriteHeader(500)
+			io.WriteString(writer, fmt.Sprintf("File %s could not be read", requestPath))
+		} else {
+			writer.Write(bytes)
+		}
+	}
+}
