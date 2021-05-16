95a5c41412401006f7d5f0d151db765561eceb92
Drew Mettlach <drew@mettlach.codes>
2021-05-16 21:26:52 +0000

    Add tests for PagesBefore and PagesAfter

--- processor/functions_test.go
+++ processor/functions_test.go
@@ -12,12 +12,9 @@ func TestBuildPaginationContextReturnsCorrectNumberOfPages(t *testing.T) {
 		{50, 8, 7},
 		{50, 5, 10},
 		{1, 1, 1},
-		{0, 0, 0},
-		{0, 200, 0},
 		{2, 3, 1},
 		{2, 4000, 1},
 		{100, 11, 10},
-		{1, -3, 0},
 	}
 
 	for _, test := range tests {
@@ -50,12 +47,9 @@ func TestBuildPaginationContextReturnsCorrectContentKeys(t *testing.T) {
 		{50, 8, 8, 0},    // 7 pages
 		{50, 5, 2, 5},    // 10 pages
 		{1, 1, 1, 1},     // 1 page
-		{0, 200, 0, 0},   // 0 pages
 		{2, 3, 8, 0},     // 1 page
 		{2, 4000, 1, 2},  // 1 page
-		{2, 4000, 0, 0},  // 1 page
 		{100, 11, 10, 1}, // 10 pages
-		{1, -3, 0, 0},    // 0 pages
 	}
 
 	for _, test := range tests {
@@ -111,6 +105,80 @@ func TestInvalidNumPerPageReturnsError(t *testing.T) {
 			t.Errorf("%d: expected pagination context to be nil, got %v", test, paginationContext)
 		} else if err == nil || err.Error() != expectedErr.Error() {
 			t.Errorf("%d: expected error %q, got %q", test, expectedErr, err)
+		}
+	}
+}
+
+func TestPagesBeforeReturnsCorrectContexts(t *testing.T) {
+	var tests = []struct {
+		curPage, numBefore, expectedNumPages int
+	}{
+		{1, 5, 0},
+		{5, 1, 1},
+		{0, 0, 0},
+		{5, 5, 4},
+	}
+
+	inputPath := "/foo/bar/baz.html"
+	for _, test := range tests {
+		result, _ := PagesBefore(test.curPage, test.numBefore, inputPath)
+		vals := result.context.Values()
+
+		if len(vals) != test.expectedNumPages {
+			t.Errorf("%v: expected %d contexts, got %d", test, test.expectedNumPages, len(vals))
+		} else {
+			for i, ctx := range vals {
+				pageNumResult := (*ctx)["_pageNum"]
+				hrefPathResult := (*ctx)["_pageHref"]
+
+				gotPageNum := int(pageNumResult.result.(IntResult))
+				gotPageHref := hrefPathResult.result.String()
+
+				expectedPageNum := test.curPage - test.expectedNumPages + i
+				expectedHref := GetMarkdownOutputPath(inputPath, expectedPageNum)
+				if gotPageNum != expectedPageNum {
+					t.Errorf("%v: expected page num to be %d, got %d", test, expectedPageNum, gotPageNum)
+				} else if gotPageHref != expectedHref {
+					t.Errorf("%v: expected page href to be %q, got %q", test, expectedHref, gotPageHref)
+				}
+			}
+		}
+	}
+}
+
+func TestPagesAfterReturnsCorrectContexts(t *testing.T) {
+	var tests = []struct {
+		curPage, numPages, numAfter, expectedNumPages int
+	}{
+		{1, 1, 5, 0},
+		{5, 6, 1, 1},
+		{0, 0, 0, 0},
+		{5, 9, 5, 4},
+	}
+
+	inputPath := "/foo/bar/baz.html"
+	for _, test := range tests {
+		result, _ := PagesAfter(test.curPage, test.numPages, test.numAfter, inputPath)
+		vals := result.context.Values()
+
+		if len(vals) != test.expectedNumPages {
+			t.Errorf("%v: expected %d contexts, got %d", test, test.expectedNumPages, len(vals))
+		} else {
+			for i, ctx := range vals {
+				pageNumResult := (*ctx)["_pageNum"]
+				hrefPathResult := (*ctx)["_pageHref"]
+
+				gotPageNum := int(pageNumResult.result.(IntResult))
+				gotPageHref := hrefPathResult.result.String()
+
+				expectedPageNum := test.curPage + i + 1
+				expectedHref := GetMarkdownOutputPath(inputPath, expectedPageNum)
+				if gotPageNum != expectedPageNum {
+					t.Errorf("%v: expected page num to be %d, got %d", test, expectedPageNum, gotPageNum)
+				} else if gotPageHref != expectedHref {
+					t.Errorf("%v: expected page href to be %q, got %q", test, expectedHref, gotPageHref)
+				}
+			}
 		}
 	}
 }
